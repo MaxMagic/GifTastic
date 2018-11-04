d50b8008d4ca054ea9888f3c0aef80f8849f787c
MaxMagic <Lennox.E.David@gmail.com>
2018-11-04 09:09:37 -0500

    updated layout

---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+/*  - Create an array of related things
+    - Create for loop to poulate page with buttons
+    - Add search bar
+    - Value in search bar is pushed to current array
+    - Will need controls for the starting and stopping of gifs
