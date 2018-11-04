62b2540be77b6a92e701dc17073acb95a172fc25
MaxMagic <Lennox.E.David@gmail.com>
2018-11-04 12:30:45 -0500

    updated logic

--- javascript/app.js
+++ javascript/app.js
@@ -4,22 +4,35 @@
     - Value in search bar is pushed to current array
     - Will need controls for the starting and stopping of gifs
 */
-let topics = ["basketball", "football"];
+var topics = ["basketball", "football"];
 
 function addButtons(){
-    for (let x = 0; x < topics.length; x++){
-       let newButton = $("<button>");
-       newButton.text(topics[x]);
-       newButton.attr("data-name", topics[x]);
-       $("#buttonArea").append(newButton); 
+  $("#buttonArea").html("");
+    for (var x = 0; x < topics.length; x++){
+      var newButton = $("<button>");
+      newButton.text(topics[x]);
+      newButton.attr("data-name", topics[x]);
+      newButton.addClass("topicButton")
+      $("#buttonArea").append(newButton); 
     }
 };
 
 $("#add-topic").on("click", function(event) {
     event.preventDefault();
+    console.log("soccer" === "soccer");
 
-    let newTopic = $("#find-topic").val().trim();
-    topics.push(newTopic);
+    var newTopic = $("#find-topic").val().trim();
+    console.log(typeof newTopic);
+    for (var i = 0; i < topics.length; i++){
+      if (topics[i] === newTopic){
+        console.log(topics[i]);
+        alert("You added that one already!");
+        $("#find-topic").val("");
+      } else {
+        topics.push(newTopic);
+        $("#find-topic").val("");
+      };
+    }
 });
 
 $(".gif").on("click", function() {
@@ -37,45 +50,45 @@ $(".gif").on("click", function() {
       }
 });    
 
-$("button").on("click", function() {
-    let searchTopic = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      searchTopic + "&api_key=PTyPj05INMiXMaDcWb36KtqE9cYwa931&rating=g&limit=10";
+$(".topicButton").on("click", function() {
+  var searchTopic = $(this).attr("data-name");
+  console.log(searchTopic);
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTopic + "&api_key=PTyPj05INMiXMaDcWb36KtqE9cYwa931&rating=g&limit=10";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
 
-        console.log(queryURL);
-        console.log(response);
+      console.log(queryURL);
+      console.log(response);
 
-        var results = response.data;
+      var results = response.data;
 
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
 
-          // Creating and storing a div tag
-          var topicDiv = $("<div>");
+        // Creating and storing a div tag
+        var topicDiv = $("<div>");
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
 
-          // Creating and storing an image tag
-          var topicImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          topicImage.attr("src", results[i].images.fixed_height_still.url);
-          topicImage.attr("data-still", results[i].images.fixed_height_still.url);
-          topicImage.attr("data-animate", results[i].images.original);
-          topicImage.attr("data-state", "still");
-          topicImage.addClass("gif");
+        // Creating and storing an image tag
+        var topicImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        topicImage.attr("src", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-animate", results[i].images.original);
+        topicImage.attr("data-state", "still");
+        topicImage.addClass("gif");
 
-          // Appending the paragraph and image tag to the animalDiv
-          topicDiv.append(p);
-          topicDiv.append(topicImage);
+        // Appending the paragraph and image tag to the animalDiv
+        topicDiv.append(p);
+        topicDiv.append(topicImage);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifArea").prepend(topicDiv);
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifArea").prepend(topicDiv);
       }
   });
 });
